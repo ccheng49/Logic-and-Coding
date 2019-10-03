f80284b39e19538653fe186476bbedb471df0436
ccheng49 <54420625+ccheng49@users.noreply.github.com>
2019-10-03 15:01:53 -0700

    Add files via upload

---
+++ Knock.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Knock{
+    public static void main(String args[]){
+        System.out.println("Knock, Knock. Who's There?");
+        Scanner text = new Scanner (System.in);
+        String b = "banana", o = "orange", guess = text.nextLine();
+        
+        if (guess.equals(o)){
+            System.out.println("Orange, You Glad You Didn't Type Banana?");
+        }
+        else
+        while(!guess.equals(o)){
+            if (guess.equals(b)){
+                System.out.println("Banana Who?");
+                break;
+            }
+            
+            else
+            System.out.println("You Are Not Playing My Game Very Well");
+            System.out.println("Knock, Knock. Who's There?");
+        }
+        
+        text.close();
+    }
+}
